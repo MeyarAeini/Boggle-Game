c55d2e2c3c32f98cbcf03737a18caf9b716834c0
MeyarAeini <mahiar.aeini@gmail.com>
2025-04-08 17:53:57 -0400

    this is a generated board by a crate publish by myself

--- boggle-board-generator/src/main.rs
+++ boggle-board-generator/src/main.rs
@@ -1,26 +1,6 @@
-
-use crate::genetic_board::GeneticBoard;
-
-use std::time::Instant;
-
-pub mod board;
-pub mod genetic_board;
-pub mod genetic_algo_board;
-
-use word_trie::builder;
 use boggle_maker::BoggleBuilder;
-use boggle_maker::Board;
 
 fn main() {
-    
-    // let trie = builder::TrieBuilder::new()
-    // .from_file("words.txt")
-    // .expect("Failed to load trie from file");
-
-    // let start = Instant::now();
-    // crate::genetic_algo_board::run(4,4,&trie);
-    // let duration = start.elapsed();
-    // println!("genetic algorithm took : {:?}",duration);
 
     //SERSPATGLINESERS
     let builder = BoggleBuilder::new()
@@ -30,11 +10,8 @@ fn main() {
     .with_width(4);
    
     if let Some(board) = builder.build().expect("Failed to load trie from words.txt file"){
-       println!("this is a generated board by a crate publish by myself {:?} with score of {}", board.to_string(),board.score().unwrap());
+       println!("this is a generated board by a crate publish by myself:");
+       println!("{:?}", board.hash().to_ascii_uppercase());
+       println!("Total score: {}", board.score().unwrap());
     }
-
-    // let start = Instant::now();
-    // let hello_board = GeneticBoard::get_board(&trie, 3000, 4,4);
-    // let duration = start.elapsed();
-    // println!("hello board, genetic generated : {}, {:?}", hello_board.hash(),duration);
 }
