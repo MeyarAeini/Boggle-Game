4e38f5ddf0eb63439689d1943f14e2589f5dd6f3
MeyarAeini <mahiar.aeini@gmail.com>
2025-04-14 21:29:55 -0400

    comment out the sync service authentication, needs more time to figure out the solution

--- boggle-game-app/app/game/components/board-update-subscribe.tsx
+++ boggle-game-app/app/game/components/board-update-subscribe.tsx
@@ -2,15 +2,15 @@
 
 import { GameSession } from "@/app/lib/definitions";
 import { useGameState } from "@/app/lib/useGameState";
-import { useSession } from "next-auth/react";
+//import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
-
+//the client session information missed, needs time to figureout how to handle it properly
 export default function BoardUpdateSubscribe({ gameSession }: { gameSession: GameSession }) {
     const { join, connected } = useGameState();
-    const { data: session, status } = useSession({ required: true });
+    //const { data: session, status } = useSession({ required: true });
     useEffect(() => {
-        join(gameSession.sessionId,session?.user?.accessToken);
-    }, [connected,status]);
+        join(gameSession.sessionId,"");//session?.user?.accessToken);
+    }, [connected]);//,status]);
     return <></>;
 }
